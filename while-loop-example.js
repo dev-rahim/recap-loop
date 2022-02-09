9725534e7325219b6d29286e6918b40d8cd2052d
dev-rahim <rahimuddin5915@gmail.com>
2022-02-09 21:10:28 +0600

    explor  while loop examples

---
+++ while-loop-example.js
@@ -0,0 +1,17 @@
+var num = 1;
+// while ( num < 11 ) {
+//     console.log( num );
+//     num++;
+// }
+
+// even numbers 
+// while (num<21) {
+//     console.log(num);
+//     num+=2;
+// }
+
+// odd numbers 
+while ( num < 21 ) {
+    console.log( num );
+    num += 2;
+}
