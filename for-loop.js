bbb9d353620f3c293b0fdc5501eb69f886c978e5
dev-rahim <rahimuddin5915@gmail.com>
2022-02-09 21:27:33 +0600

    explor for loop examples

---
+++ for-loop.js
@@ -0,0 +1,11 @@
+// for(var roastGiven=0; roastGiven <7; roastGiven++){
+//     console.log("roast den, please!!");
+// }
+// odd numbers 
+// for ( var num = 1; num < 20; num += 2 ) {
+//     console.log( num );
+// }
+// even numbers 
+for ( var num = 2; num < 20; num += 2 ) {
+    console.log( num );
+}
