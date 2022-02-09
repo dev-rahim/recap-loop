bfd1291ce76b9cefde04ce11532ce56d8f9cb867
dev-rahim <rahimuddin5915@gmail.com>
2022-02-09 21:51:39 +0600

    explor array with loop examples

--- array-loops.js
+++ array-loops.js
@@ -1,1 +1,11 @@
-var
+var numbers = [ 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 ];
+// for ( var i = 0; i < 6; i++ ) {
+//     console.log( numbers[ i ] );
+// }
+for ( var i = 0; i < numbers.length; i++ ) {
+    console.log( numbers[ i ] );
+}
+
+var num1 = "99.5";
+var num2 = 15;
+console.log( num1 + num2 );
